86c036976cb9b6387e70becfc7c10ae733d24ad6
ChadGessner <CGess83@me.com>
2022-10-26 17:35:00 -0400

    Had to fix something real quick...Add project files.

---
+++ shoppingListLab/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoppingListLab
+{
+    internal enum Item
+    {
+        AppleSauce,
+        SnickersBar,
+        AppleFlavoredHorsePaste,
+        ToiletPaper,
+        Sand,
+        Water,
+        FrozenChickenBurgers,
+        Bleach
+
+    }
+}
